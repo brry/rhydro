6ddd2cf0c89a173e184b98a24c639853e801aa0d
brry <berryboessenkool@hotmail.com>
2017-01-04 14:54:13 +0100

    checkpc: in a function, package versions now checked

--- checkpc.R
+++ checkpc.R
@@ -1,6 +1,8 @@
 
 # check if laptop is set up for R short course at EGU 2017
 
+checkpcfun <- function(...) # to keep everything within a function environment
+{
 link <- " visit \nbrowseURL('https://github.com/brry/rhydro#install')\n"
 
 # R version
@@ -12,16 +14,31 @@ if(getRversion() < "3.3.2") message("Note: Your ",R.version.string, " is outdate
 # Rstudio
 if(!"tools:rstudio" %in% search()) message("Note: you are not using Rstudio. Strongly recommended!", link)
 
+
 # Packages
 packs <- c("hydroGOF")#, "dummy")
+message("Getting CRAN package versions, please wait ca 1-5 seconds...\n")
+cranpacks <- available.packages()
 for (p in packs)
   {
   if(!requireNamespace(p, quietly=TRUE)) stop("package '", p, "' is not installed. ",
                                               "Run \ninstall.packages('",p,"')", call.=FALSE)
+  cranv <- cranpacks[p,]["Version"]
+  packv <- packageVersion(p)
+  if(packv<cranv) message("Note: package '",p,"' (",packv,") is not up to date ",
+                           "(CRAN version is ",cranv,"). You can run \ninstall.packages('",p,"')\n")
+  unloadNamespace(p) 
+  #detach(paste0("package:",p), character.only=TRUE, unload=TRUE)
   }
+
 
 # Success:
 message("\nThere have been no errors, thus you should be all set to go.\n",
         "We're looking forward to seeing you at EGU.\n",
+        "If not already done, please register via berry-b@gmx.de\n",
         "Please do not forget your laptop and come early, if possible.\n",
-        "If you want to refresh your basic R skills, please", link)
+        "If you want to refresh your basic R skills, you can", link)
+
+}
+checkpcfun()
+rm(checkpcfun)
