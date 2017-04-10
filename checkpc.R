4d5d11aa74584063f29b26f78bbbedba403658e3
brry <berryboessenkool@hotmail.com>
2017-04-10 14:18:59 +0200

    package tests expanded + vectorized

--- checkpc.R
+++ checkpc.R
@@ -3,38 +3,52 @@
 
 checkpcfun <- function(...) # to keep everything within a function environment
 {
-link <- " visit \nbrowseURL('https://github.com/brry/rhydro#install')\n"
+link <- " Visit \nbrowseURL('https://github.com/brry/rhydro#install')\n"
 
+message("* Checking R version and Rstudio...")
 # R version
 if(getRversion() < "3.3") stop("Your ",R.version.string, " is too old for the course.", link, call.=FALSE)
 
 if(getRversion() < "3.3.2") message("Note: Your ",R.version.string, " is outdated. ", 
                                     "The current version is 3.3.2 (2016-10-31).",link)
-
 # Rstudio
 if(!"tools:rstudio" %in% search()) message("Note: you are not using Rstudio. Strongly recommended!", link)
 
 
-# Packages
-packs <- c("hydroGOF")#, "dummy")
-message("Getting CRAN package versions, please wait ca 1-5 seconds...\n")
+# Packages ---
+
+message("* Checking whether needed packages are installed...")
+
+p <- "OSMscale"
+if(!requireNamespace(p, quietly=TRUE)) stop("package '", p, "' is not installed. ",
+   "Please run \ninstall.packages('",p,"')\nIf that fails, read the instructions at ",
+   "\nhttps://github.com/brry/OSMscale#installation", call.=FALSE)
+rm(p)
+
+packs <- c("hydroGOF","airGR","leaflet","rgdal","mapview","sf")
+inst <- sapply(packs, function(p) requireNamespace(p, quietly=TRUE) )
+if(any(!inst)) stop("You need to install some packages for the course. Please run\n",
+                    "install.packages(c('",paste0(packs[!inst],collapse="','"),"'))", call.=FALSE)
+
+message("* Checking package versions, please wait ca 1-5 seconds...")
 cranpacks <- available.packages()
-for (p in packs)
+outdated <- sapply(packs, function(p) 
   {
-  if(!requireNamespace(p, quietly=TRUE)) stop("package '", p, "' is not installed. ",
-                                              "Run \ninstall.packages('",p,"')", call.=FALSE)
   cranv <- cranpacks[p,]["Version"]
   packv <- packageVersion(p)
   if(packv<cranv) message("Note: package '",p,"' (",packv,") is not up to date ",
-                           "(CRAN version is ",cranv,"). You can run \ninstall.packages('",p,"')\n")
-  unloadNamespace(p) 
-  #detach(paste0("package:",p), character.only=TRUE, unload=TRUE)
-  }
+                           "(CRAN version is ",cranv,").")
+  packv<cranv 
+  })
+if(any(outdated)) message("You can run \ninstall.packages(c('",
+                          paste0(packs[outdated],collapse="','"),"'))")
+# unload packages again:
+##dummy <- sapply(packs, function(p) try(unloadNamespace(p), silent=TRUE))
 
 
 # Success:
-message("\nThere have been no errors, thus you should be all set to go.\n",
-        "We're looking forward to seeing you at EGU.\n",
+message("\n* There have been no errors, thus you should be all set to go.\n",
+        "We're looking forward to seeing you at EGU on Monday at 15:30 in Room -2.31.\n",
         "If not already done, please register via berry-b@gmx.de\n",
         "Please do not forget your laptop and come early, if possible.\n",
         "If you want to refresh your basic R skills, you can", link)
