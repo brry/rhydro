33ade65e7f97cf7fb1d4694086659231ac792b33
brry <berry-b@gmx.de>
2017-04-22 14:24:52 +0200

    RColorBrewer added

--- checkpc.R
+++ checkpc.R
@@ -35,7 +35,7 @@ if(!requireNamespace("sf", quietly=TRUE)) {errors <- TRUE ; sf <- TRUE}
 
 packs <- c("hydroGOF","airGR","leaflet","rgdal","mapview",
            "sf","OSMscale","dygraphs","extremeStat", 
-           "geoR", "animation", 
+           "geoR", "animation", "RColorBrewer",
            "xts", "trend", "Kendall", "hydroTSM") 
 inst <- sapply(packs, function(p) requireNamespace(p, quietly=TRUE) )
 if(any(!inst)) errors <- TRUE
